f9400632160cc4cc8e746b0a8de13ae872dc8f98
Cherdintsev <alexander.cherdincev@gmail.com>
2023-09-21 07:56:56 +0200

    bcumi_dupd_covd.py is somewhat finished, now to test-run

--- bcumi_dupd_covd.py
+++ bcumi_dupd_covd.py
@@ -1,83 +1,115 @@
-## covv output from chunks to [starts_out, ends_out]
-## cov for BC => from creating additional heavy list to reorganizing what we have
-## output bc umi chr? strand? positions which went through filters, or better chunks
+## CODE IS TO BE REVISED IF THERE ARE > FLAGS(<256) THAN 0 AND 16 
 
+## output bc umi chr? strand? positions which went through filters, or better chunks
 #### SAVE COUNTED CIGAR AND TRANSMIT TO THE NEXT STEP!!!
+
 import sys
 from collections import defaultdict
 import re
+import bisect
 
 def cigar2pos(start_cigar): 
-	start, cigar = start_cigar
-	pos = int(start)
-	starts = list() #tuple()
-	ends = list() #tuple()
+	pos, cigar = start_cigar
+	starts = list(); ends = list() 
 	cigar_ops = pattern.findall(cigar) 
 	if cigar_ops[0][1] == 'S':
 		cigar_ops = cigar_ops[1:]
 	for length, op in cigar_ops:
 		length = int(length)
 		if op == 'M':
-			starts.append(pos) #+= (pos, )
-			ends.append(pos+length) #+= (pos + length, )
+			starts.append(pos) 
+			ends.append(pos+length) 
 		elif op == 'I':
 			continue
 		pos += length
 	return(starts, ends) 
 
-def covv(startss, endss, th): 
-	startss = sorted(startss, reverse=True)
-	endss = sorted(endss, reverse=True)
-	starts = startss.pop()
-	ends = endss.pop()
-	cov = 0
-	chunk = False
-	cov5 = list() #tuple()
-	#starts_out = list()
-	#ends_out = list()
-	while startss:
-		if starts < ends:
-			cov += 1
-			if cov >= th and not chunk: #switch_cov5:
-				chunk = [starts] # True #(starts,)
-				#starts_out.append(starts)
-			starts = startss.pop()
-		elif starts > ends:
-			cov -= 1
-			if chunk and cov < th : #switch_cov5
-				chunk.append(ends) #+= (ends,)
-				#ends_out.append(ends)
-				cov5.append(chunk) #+= (chunk) #cov5 += chunk
-				chunk = False
-			ends = endss.pop()
-		elif starts == ends:
-			starts = startss.pop()
-			ends = endss.pop()
-		else:
-			print("error", starts, ends)
-			exit()
+def find_intersect(starts, ends, th):
+    n = len(starts)
+    result_starts = []; result_ends = []
+    i = 0; j = 0; overlap_count = 0
 
-	while cov >= th-1:
-		if starts < ends:
-			cov += 1
-			if cov >= th and not chunk: 
-				chunk = [starts] # True #(starts,)
-			starts=endss[0]
-		cov -= 1
-		if chunk and cov < th : 
-			chunk.append(ends) # += (ends,)
-			#ends_out.append(ends)
-			cov5.append(chunk) # += (chunk, )
-			chunk = False
-		ends = endss.pop()
-	#if starts_out:
-	#	return([starts_out, ends_out]) #cov5)
-	#else:
-	#	return(False)
-	return(cov5)
+    while i < n and j < n:
+        if starts[i] <= ends[j]:
+            overlap_count += 1
+            if overlap_count == th:
+                result_starts.append(starts[i])
+            i += 1
+        else:
+            overlap_count -= 1
+            if overlap_count == th-1:
+                result_ends.append(ends[j])
+            j += 1
+
+    if result_starts:
+        return result_starts, result_ends
+    else:
+        return
+
+def íntersect(startss_endss, th):
+    starts = list(); ends = list()
+    for starts_ends in startss_endss:
+        for start in starts_ends[0]:
+            bisect.insort(starts, start)
+        for end in starts_ends[1]:
+            bisect.insort(ends, end)
+    return find_intersect(starts, ends, th)
+
+def proc():
+	global out, bcumis
+	for _bc in out:
+		for _umi in out[_bc]:
+			if len(out[_bc][_umi]) > 1:
+				bcumis[_bc][_umi] = False # if 5+ 5- and 5 next chr
+				continue
+			_strand = list(out[_bc][_umi].keys())[0]
+			if len(out[_bc][_umi][_strand]) < dup_min:
+				bcumis[_bc][_umi] = False
+				continue
+			if (_bc not in bcumis) or (_umi not in bcumis[_bc]):
+				starts_ends_dups = list()
+				for start_cigar in out[_bc][_umi][_strand]:
+					starts_ends_dup = cigar2pos(start_cigar)
+					starts_ends_dups.append(starts_ends_dup)
+				starts_ends_umi = íntersect(starts_ends_dups, th = dup_min-1 )
+				if starts_ends_umi:
+					bcumis[_bc][_umi] = (_strand, starts_ends_umi)
+			else:
+				bcumis[_bc][_umi] = False
+
+		if len([i for i in bcumis[_bc].values() if i])<cov_min:
+			#del bcumis[_bc]
+			for _umi in  bcumis[_bc]:
+				bcumis[_bc][_umi] = False
+			continue
+
+		starts_ends_umis = defaultdict(lambda: list())
+		for _umi in  bcumis[_bc]:
+			if not bcumis[_bc][_umi]:
+				continue
+			_strand, starts_ends_umi = bcumis[_bc][_umi]
+			starts_ends_umis[_strand].append(starts_ends_umi)
+
+		starts_ends_chrbc = dict()
+		for _strand in starts_ends_umis:
+			starts_ends_chrbc[_strand] = íntersect(starts_ends_umis[_strand], th=cov_min)
+
+		for _umi in bcumis[_bc]: 
+			if not bcumis[_bc][_umi]:
+				continue
+			_strand, starts_ends_umi = bcumis[_bc][_umi]
+			coords = íntersect(starts_ends_chrbc[_strand], starts_ends_umi, th=1)
+			if coords:
+				bcumis[_bc][_umi] = (chrom_old, _strand, coords)
+			else:
+				bcumis[_bc][_umi] = False
+
+		if len([i for i in bcumis[_bc].values() if i])<cov_min:
+			for _umi in  bcumis[_bc]:
+				bcumis[_bc][_umi] = False
+			continue
 
 pattern = re.compile(r'(\d+)([MIDNS])') 
-
 
 #th = int(sys.argv[1])
 dup_min = int(sys.argv[1]) #5
@@ -101,105 +133,36 @@ chrom_old=''
 
 for line in sys.stdin:
 	line = line.strip().split() #"\t"
-	if (line[-2]!="CB:Z:-" and line[-1]!="UB:Z:-" and line[4]=="255" and line[2]!="chrM" and int(line[1])<256):
-		bc = line[-2][5::] 
-		if bc in filt_bcs_d:
+	if not (line[-2]!="CB:Z:-" and line[-1]!="UB:Z:-" and line[4]=="255" and line[2]!="chrM" and int(line[1])<256):
+		continue
+	bc = line[-2][5::] 
+	if bc not in filt_bcs_d:
+		continue
 
-			bc = filt_bcs_d[bc] # replacing bc with its index (string to int)
-			umi = line[-1][5::]
-			strand = int(line[1]) # 0 forward, 16 reverse; could there be other flags with count>th?
-			chrom = line[2]
+	bc = filt_bcs_d[bc] # replacing bc with its index (string to int)
+	umi = line[-1][5::]
+	strand = int(line[1]) # 0 forward, 16 reverse; could there be other flags with count>th?
+	
+	chrom = line[2] # below replacing chrom with its index
+	if chrom not in chroms:
+		chroms[chrom] = chrom_ind
+		chrom_ind += 1
+	chrom = chroms[chrom]
 
-			# below replacing chrom with its index
-			if chrom not in chroms:
-				chroms[chrom] = chrom_ind
-				chrom_ind += 1
-			chrom = chroms[chrom]
+	start = int(line[3]); cigar = line[5]
 
-			start = int(line[3]); cigar = line[5]
+	if chrom == chrom_old:
+		out[bc][umi][strand].append( (start, cigar) )
+	elif chrom_old:
+		proc()
+		out = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: list())))
+		out[bc][umi][strand].append( (start, cigar) ) 
+		chrom_old = chrom 
+	else: # for the first round (chrom_old == False)
+		out[bc][umi][strand].append( (start, cigar) ) 
+		chrom_old = chrom 
 
-			if chrom == chrom_old:
-				out[bc][umi][strand].append( (start, cigar) ) # += 1 
-			elif chrom_old:
-				for _bc in out:
-					for _umi in out[_bc]:
-						if len(out[_bc][_umi]) > 1:
-							continue
-							# if there are 5+ 5- and 5 of next chr, will pass
-							# otherwise bcumis[_bc][_umi] = False needed
-						_strand = list(out[_bc][_umi].keys())[0]
-						#for _strand in out[_bc][_umi]:
-						if len(out[_bc][_umi][_strand]) >= dup_min:
-							#print(_bc, _umi)
-							# stuff below works for strand check as well
-							# as long as there are only 0 and 16 flags
-							# 0 forward, 16 reverse
-							# could there be other flags with count>th?
-							#if (_bc, _umi) not in bcumis:
-							if (_bc not in bcumis) or (_umi not in bcumis[_bc]):
-								startss = list() 
-								endss = list()
-								for start_cigar in out[_bc][_umi][_strand]:
-									starts, ends = cigar2pos(start_cigar)
-									# in c++ better save not absolute but relative nrs, 
-									# since size of uint8 << uint32
-									# BUT HERE WE ALSO COULD INTRODUCE IT VIA NUMPY
-									# not much help, np.uint8 25bytes, np.uint32 28bytes
-									startss.extend(starts) #+= (*starts,)
-									endss.extend(ends)
-								chunks = covv(startss, endss, th = dup_min-1 )
-								if chunks:
-									bcumis[_bc][_umi] = (_strand, chunks)
-							else:
-								#bcumis[_bc][_umi] = False
-								#thus we could make loops easier but if there are 5+ 5- and 5 of next chr, will pass
-								del bcumis[_bc][_umi]
-
-					if len(bcumis[_bc])<cov_min:
-						del bcumis[_bc]
-					else:
-						startss = defaultdict(lambda: list())
-						endss = defaultdict(lambda: list())
-						for _umi in  bcumis[_bc]:
-							_strand, chunks = bcumis[_bc][_umi] #starts_ends
-							for chunk in chunks:
-								startss[_strand].append(chunk[0])
-								endss[_strand].append(chunk[1])
-							#starts, ends = starts_ends
-							#startss[_strand].extend(starts)
-							#endss[_strand].extend(ends)
-
-						starts_ends = dict() #chunks
-						for _strand in startss:
-							chunkss = covv(startss[_strand], endss[_strand], th=cov_min)
-							starts_ends[_strand] = zip(*chunkss)
-
-					for _umi in bcumis[_bc]: #out[_bc]
-						_strand, umi_chunks = bcumis[_bc][_umi] #list(out[_bc][_umi].keys())[0]
-						uc_starts, uc_ends = zip(*umi_chunks)
-						uc_starts.extend(starts_ends[_strand][0])
-						uc_ends.extend(starts_ends[_strand][1])
-						coords = covv(uc_starts, uc_ends, th=1)
-						if coords:
-							bcumis[_bc][_umi] = (chrom_old, _strand, coords)
-						else:
-							bcumis[_bc][_umi] = False
-
-				out = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: list())))
-				out[bc][umi][strand].append( (start, cigar) ) #  += 1
-				chrom_old = chrom #; print('!=,+')
-			else: # for the first round (chrom_old == False)
-				out[bc][umi][strand].append( (start, cigar) ) #  += 1
-				chrom_old = chrom #; print('!=,-')
-
-for _bc in list(out.keys()):
-	for _umi in out[_bc]:
-		for _strand in out[_bc][_umi]:
-			if len(out[_bc][_umi][_strand]) >= dup_min:
-				if (_bc, _umi) not in bcumis:
-					bcumis[(_bc, _umi)] = (chrom_old, _strand)
-				else:
-					bcumis[(_bc, _umi)] = False
+proc()
 
 
 for bc in bcumis:
