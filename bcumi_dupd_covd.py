126caf8cbaa74a8a1e97bfa71da0f5812077197d
Cherdintsev <alexander.cherdincev@gmail.com>
2023-09-21 12:43:02 +0200

    bcumi_dupd_covd.py is fixed and running, 6GB RAM and 1119s for AL4_yng, but maybe bcumis[bc][umi] to [bc, umi]

--- bcumi_dupd_covd.py
+++ bcumi_dupd_covd.py
@@ -26,10 +26,13 @@ def cigar2pos(start_cigar):
 
 def find_intersect(starts, ends, th):
     n = len(starts)
+    starts.append(ends[-1]+1)
+
     result_starts = []; result_ends = []
     i = 0; j = 0; overlap_count = 0
 
-    while i < n and j < n:
+    while i <= n and j < n:
+        #print(result_starts, result_ends, i,j,n,overlap_count)
         if starts[i] <= ends[j]:
             overlap_count += 1
             if overlap_count == th:
@@ -39,6 +42,8 @@ def find_intersect(starts, ends, th):
             overlap_count -= 1
             if overlap_count == th-1:
                 result_ends.append(ends[j])
+                if i == n:
+                    break
             j += 1
 
     if result_starts:
@@ -48,15 +53,23 @@ def find_intersect(starts, ends, th):
 
 def íntersect(startss_endss, th):
     starts = list(); ends = list()
+    #print('ss',startss_endss)
     for starts_ends in startss_endss:
+        #print('s', starts_ends)
         for start in starts_ends[0]:
             bisect.insort(starts, start)
         for end in starts_ends[1]:
             bisect.insort(ends, end)
+    #print('in', startss_endss, starts, ends)
+    #if len(starts) != len(ends):
+    #	print(startss_endss, starts, ends)
+    #	exit()
+    #print(find_intersect(starts, ends, th))
+    #exit()
     return find_intersect(starts, ends, th)
 
 def proc():
-	global out, bcumis
+	global out, bcumis, chrom_old
 	for _bc in out:
 		for _umi in out[_bc]:
 			if len(out[_bc][_umi]) > 1:
@@ -71,43 +84,78 @@ def proc():
 				for start_cigar in out[_bc][_umi][_strand]:
 					starts_ends_dup = cigar2pos(start_cigar)
 					starts_ends_dups.append(starts_ends_dup)
+				#print(starts_ends_dups)
+				#exit()
+				#print("dups2umi")
 				starts_ends_umi = íntersect(starts_ends_dups, th = dup_min-1 )
+				#print('out', starts_ends_umi)
 				if starts_ends_umi:
-					bcumis[_bc][_umi] = (_strand, starts_ends_umi)
+					bcumis[_bc][chrom_old][_umi] = (_strand, starts_ends_umi)
 			else:
 				bcumis[_bc][_umi] = False
 
-		if len([i for i in bcumis[_bc].values() if i])<cov_min:
+		if chrom_old not in bcumis[_bc]:
+			continue
+		#else:
+		#	print('1')
+		if len(bcumis[_bc][chrom_old])<cov_min:
+			#print('len small 1')
 			#del bcumis[_bc]
-			for _umi in  bcumis[_bc]:
+			for _umi in  bcumis[_bc][chrom_old]:
 				bcumis[_bc][_umi] = False
 			continue
 
+		#len(bcumis[_bc][_umi])==3
+		# that's horrific, 
+		#after each chr it would check ALL the umis of each BC present,
+		# instead of only working with the output from last chr
+		# FIXED !! intermediate bcumis[_bc][chrom_old]
+
 		starts_ends_umis = defaultdict(lambda: list())
-		for _umi in  bcumis[_bc]:
-			if not bcumis[_bc][_umi]:
-				continue
-			_strand, starts_ends_umi = bcumis[_bc][_umi]
+		for _umi in  bcumis[_bc][chrom_old]:
+			#if not bcumis[_bc][_umi] or len(bcumis[_bc][_umi])==3:
+			#	continue
+			_strand, starts_ends_umi = bcumis[_bc][chrom_old][_umi]
+			#print(starts_ends_umi)
 			starts_ends_umis[_strand].append(starts_ends_umi)
 
 		starts_ends_chrbc = dict()
 		for _strand in starts_ends_umis:
+			#print("umis2chrbc")
+			#print(starts_ends_umis[_strand])
 			starts_ends_chrbc[_strand] = íntersect(starts_ends_umis[_strand], th=cov_min)
-
-		for _umi in bcumis[_bc]: 
-			if not bcumis[_bc][_umi]:
+		for _umi in list(bcumis[_bc][chrom_old].keys()): 
+			#if not bcumis[_bc][_umi]  or len(bcumis[_bc][_umi])==3:
+			#	continue
+			_strand, starts_ends_umi = bcumis[_bc][chrom_old][_umi]
+			#coords = íntersect(starts_ends_chrbc[_strand], starts_ends_umi, th=1)
+			#print(starts_ends_chrbc[_strand], starts_ends_umi)
+			#exit()
+			if not starts_ends_chrbc[_strand]:
+				bcumis[_bc][_umi] = False
+				del bcumis[_bc][chrom_old][_umi]
 				continue
-			_strand, starts_ends_umi = bcumis[_bc][_umi]
-			coords = íntersect(starts_ends_chrbc[_strand], starts_ends_umi, th=1)
+			coords = íntersect([starts_ends_chrbc[_strand], starts_ends_umi], th=1)
 			if coords:
-				bcumis[_bc][_umi] = (chrom_old, _strand, coords)
+				bcumis[_bc][chrom_old][_umi] = (_strand, coords)
 			else:
 				bcumis[_bc][_umi] = False
+				del bcumis[_bc][chrom_old][_umi]
 
-		if len([i for i in bcumis[_bc].values() if i])<cov_min:
-			for _umi in  bcumis[_bc]:
+
+		if len(bcumis[_bc][chrom_old])<cov_min:
+			#print('len small 2')
+			for _umi in  bcumis[_bc][chrom_old]:
 				bcumis[_bc][_umi] = False
-			continue
+		else:
+			#print(2)
+			for _umi in  bcumis[_bc][chrom_old]:
+				bcumis[_bc][_umi] = True #(chrom_old, _strand, coords)
+				_bc_ = l_bcs[_bc]; _chr = list(chroms.keys())[chrom_old]
+				_umi_ = ''.join([d_let_inv[i] for i in list(str(_umi))])
+				print(_bc_, _chr, _strand, _umi_, *coords )
+		del bcumis[_bc][chrom_old]
+
 
 pattern = re.compile(r'(\d+)([MIDNS])') 
 
@@ -115,20 +163,24 @@ pattern = re.compile(r'(\d+)([MIDNS])')
 dup_min = int(sys.argv[1]) #5
 cov_min = int(sys.argv[2]) #5
 
-filt_bcs=sys.argv[2]
+filt_bcs=sys.argv[3]
 filt_bcs_d=dict()
 with open(filt_bcs) as f:
-	ind = 1
+	ind = 0
 	for line in f.readlines():
 		bc = line.strip()
 		filt_bcs_d[bc] = ind
 		ind += 1
+l_bcs = list(filt_bcs_d.keys())
 
 chroms = dict()
-chrom_ind = 1
+chrom_ind = 0
+d_let = {'A':'1','C':'2','G':'3','T':'4'}
+d_let_inv = {v:k for k, v in d_let.items()}
 
-bcumis = defaultdict(lambda: dict() )
+bcumis = defaultdict(lambda: defaultdict(lambda: dict() ))
 out = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: list()))) #, tuple()
+
 chrom_old=''
 
 for line in sys.stdin:
@@ -140,8 +192,8 @@ for line in sys.stdin:
 		continue
 
 	bc = filt_bcs_d[bc] # replacing bc with its index (string to int)
-	umi = line[-1][5::]
-	strand = int(line[1]) # 0 forward, 16 reverse; could there be other flags with count>th?
+	umi = int(''.join([d_let[i] for i in list(line[-1][5::])]))
+	strand = int(line[1][0]) # 0 forward, 16 reverse; could there be other flags with count>th?
 	
 	chrom = line[2] # below replacing chrom with its index
 	if chrom not in chroms:
@@ -155,6 +207,8 @@ for line in sys.stdin:
 		out[bc][umi][strand].append( (start, cigar) )
 	elif chrom_old:
 		proc()
+		#if chrom == "chr3":
+		#	exit()
 		out = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: list())))
 		out[bc][umi][strand].append( (start, cigar) ) 
 		chrom_old = chrom 
@@ -165,8 +219,13 @@ for line in sys.stdin:
 proc()
 
 
+exit()
+
+l_chrs = list(chroms.keys())
 for bc in bcumis:
 	for umi in bcumis[bc]:
-		if bcumis[bc][umi];
+		if bcumis[bc][umi]:
 			chrom, strand, coords = bcumis[bc][umi]
-			print(bc, chrom, strand, umi, *coords )
+			_bc = l_bcs[bc]; _chr = l_chrs[chrom]
+			_umi = ''.join([d_let_inv[i] for i in list(str(umi))])
+			print(_bc, _chr, strand, umi, *coords )
